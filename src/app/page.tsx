f7daf61831ba2a629ce92af4ab349c9acabcea93
Bruno Vitte <bruno_vitte@hotmail.com>
2024-07-24 12:55:07 -0600

    initial commit setup for everything

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import CaveScapeGame from "@/components/cave-scape-game/cave-scape-game.component";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <CaveScapeGame />
+    </>
+  );
+}
---
+++ src/components/cave-scape-game/cave-scape-game.component.tsx
@@ -0,0 +1,8 @@
+
+export default function CaveScapeGame() {
+  return (
+    <div>
+      Hello World
+    </div>
+  )
+}
